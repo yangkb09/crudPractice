bbd2d5d9090462b316f0dba3c209510d6d1cb8b1
Katarina Yang <yangkb09@gmail.com>
2021-08-24 11:58:00 -0400

    feat: tier 1, create seed data

--- seed.js
+++ seed.js
@@ -1,6 +1,48 @@
 const { green, red } = require('chalk');
 const { db, Project, Robot } = require('./server/db');
 
+const robots = [{
+  name: 'KAY/O',
+  imageUrl: 'https://www.pcinvasion.com/wp-content/uploads/2021/06/VALORANT_KAY_0_Wallpapers_4k-scaled-1.jpg',
+  fuelType: 'electric',
+  fuelLevel: 88.34
+}, {
+  name: 'Orianna',
+  imageUrl: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Orianna_8.jpg',
+  fuelType: 'diesel',
+  fuelLevel: 50.60
+}, {
+  name: 'Blitzcrank',
+  imageUrl: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Blitzcrank_0.jpg',
+  fuelType: 'electric',
+  fuelLevel: 99.99
+}, {
+  name: 'Claptrap',
+  imageUrl: 'https://assets2.rockpapershotgun.com/claptrap.jpg/BROK/resize/1920x1920%3E/format/jpg/quality/80/claptrap.jpg',
+  fuelType: 'gas',
+  fuelLevel: 32.07
+}]
+
+const projects = [{
+  title: 'Add WD-40 to elbows and knees',
+  deadline: new Date(2021, 12, 31),
+  priority: 6,
+  completed: false,
+  description: 'Self care is important, even when you are a bot'
+}, {
+  title: 'Pretend we are not bots',
+  deadline: new Date(2021, 11, 1),
+  priority: 7,
+  completed: false,
+  description: 'Try to act like humans'
+}, {
+  title: 'Neutralize radiants',
+  deadline: new Date(2022, 1, 10),
+  priority: 10,
+  completed: true,
+  description: 'Infiltrate Kingdom container ship to save Earth'
+}]
+
 const seed = async () => {
   try {
     await db.sync({ force: true });
